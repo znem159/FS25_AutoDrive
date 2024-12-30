b19234af50d25f1b901db58e790a77e4e79cdd00
Thomas Matern <matern.nz@gmail.com>
2024-12-31 12:10:32 +1300

    Remove remaining return-statements from overlapBox callbacks. Fixes #53

--- scripts/Sensors/CollSensorSplit.lua
+++ scripts/Sensors/CollSensorSplit.lua
@@ -81,14 +81,11 @@ function ADCollSensorSplit:collisionTestCallback(transformId)
         if collisionObject ~= self and collisionObject ~= self.vehicle and not AutoDrive:checkIsConnected(self.vehicle:getRootVehicle(), collisionObject) then
             if unloadDriver == nil or (collisionObject ~= unloadDriver and (not AutoDrive:checkIsConnected(unloadDriver:getRootVehicle(), collisionObject))) then
                 self.newHit = true
-                -- return true
             end
         end
     else
         self.newHit = true
-        -- return true
     end
-    -- return false
 end
 
 function ADCollSensorSplit:buildBoxShape(x, y, z, width, height, length, vecZ, vecX)
